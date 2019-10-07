4031c3ad89894d00ba0655d02b61bc8a9d7d715b
RivannaDev <51270972+rivannadev@users.noreply.github.com>
2019-10-07 22:05:38 +0200

    pop

---
+++ staff/nacho-rivero/array-functions/pop.demo.js
@@ -0,0 +1,20 @@
+console.log('DEMO: pop');
+
+    var array = [1, 2, 3];
+
+        console.log('introduced array', array);
+        console.log('pop method');
+
+    var result = pop(array);
+
+        console.log('removed value: ' + result);
+
+        console.log('remaining: ' + array);
+
+        console.log('pop results');
+
+    result = pop(array);
+
+        console.log('removed value: ' + result);
+
+        console.log('remaining: ' + array);
---
+++ staff/nacho-rivero/array-functions/pop.js
@@ -0,0 +1,18 @@
+/**
+ * The pop() method removes the last element of an
+ * array, and returns that element.
+ * @param {Array} array The array to pop elements from.
+ * 
+ * @returns {last} Returns the array without the last element.
+ */
+
+    function pop(array) {
+
+        var last = array[array.length - 1];
+     
+        array.length = array.length - 1;
+     
+        return last; 
+     
+     }
+     
