6156f092184a6a27a69b769f61e3d46936daa103
Adrián Campo <adribdn7@gmail.com>
2019-10-07 19:52:28 +0200

    add extra function not finished

---
+++ staff/adri-campo/array-functions/some-demo.js
@@ -0,0 +1,9 @@
+// console.log('DEMO some');
+// var array = [1, 2, 3, 4];
+
+// console.log('');
+// console.log(some(array, function(arr, i){
+//        if(arr[i] < 3)
+// })); 
+
+
---
+++ staff/adri-campo/array-functions/some.js
@@ -0,0 +1,16 @@
+// El método some() comprueba si al menos un elemento del array 
+// cumple con
+//  la condición implementada por la función proporcionada.
+//  /**
+//  * Pushes a variable number of items into an array.
+//  * 
+//  * @param {Array} array The array to push elements to.
+//  * @param {...any} item The item (or items) to push.
+//  * 
+//  * @returns {number} The new lenth of the array.
+//  */
+// function some(array, expression ) { 
+//     for (var i = 1; i < arguments.length; i++) {
+//         expression(array, i)
+//     }
+// }
