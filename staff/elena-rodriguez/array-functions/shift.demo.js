236e567082a747050c4e846a0b92a12ff1b2f427
erodrial <elena.erodrial@gmail.com>
2019-10-07 19:36:24 +0200

    add files shift

---
+++ staff/elena-rodriguez/array-functions/shift.demo.js
@@ -0,0 +1,8 @@
+console.log('DEMO shift');
+
+var array = [1, 2, 3, 4];
+
+console.log('should delete the first item and return it and modify the original array');
+console.log(shift(array)); // 1
+console.log(array); // [2, 3, 4]
+
