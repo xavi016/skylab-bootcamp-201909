a74942d83d5e23bc9370b5302436927bfcb9a5ba
Manuel Barzi <manuelbarzi@gmail.com>
2019-10-07 16:28:55 +0200

    update array functions demos

--- staff/manuel-barzi/array-functions/push.js
+++ staff/manuel-barzi/array-functions/push.js
@@ -1,3 +1,11 @@
+/**
+ * Pushes a variable number of items into an array.
+ * 
+ * @param {Array} array The array to push elements to.
+ * @param {...any} item The item (or items) to push.
+ * 
+ * @returns {number} The new lenth of the array.
+ */
 function push(array) { 
 	//console.log(arguments) 
 
