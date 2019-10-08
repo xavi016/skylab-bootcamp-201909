34b904d66b86681f093c8d8d30283e3bf5490058
martapvalls <marpeva14@hotmail.com>
2019-10-08 22:29:52 +0200

    add new array function

--- staff/marta-penya/array-functions/reverse.js
+++ staff/marta-penya/array-functions/reverse.js
@@ -1,71 +1,19 @@
 
 
-
-
-
-
 var lenguajes = ["PHP", "C", "C#", "Rust", "Java", "JavaScript", "C++", "Go"];
 
-
-function invert(array){
-    var cadenaInvertida = [];
-    for(let i = 0; i < array.lenght; i--){
-        cadenaInvertida = array[i]
+function reverse(array) {
+    var reversed = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        reversed[i] = array[array.length - i - 1];
+       
     }
-    return cadenaInvertida
+    return reversed;
 }
-
-console.log(lenguajes);
-
-invert(lenguajes);
-console.log(lenguajes);
-
-
-
-function invert(array) {
-    var x = array.length;
-    var cadenaInvertida = [];
-   
-    while (x >= 0) {
-      cadenaInvertida = cadenaInvertida + array[i];
-      x--;
-    }
-    return cadenaInvertida;
-  }
-
-
-  console.log(lenguajes);
-
-  invert(lenguajes);
-  console.log(lenguajes);
+reverse(lenguajes);
 
 
 
 
 
-
-
-
-
-
-
-function invert(array){
-    var invertLong = array.length;
-
-    for(var i = 0; i < array.length / 2; i++){
-        console.log(array.length / 2)
-        var temporal = array[i];
-        console.log(temporal)
-        var indexInverted = invertLong - i - 1;
-        array[i] = invertLong[indexInverted];
-        array[indexInverted] = temporal;
-    }
-}
-
-
-var lenguajes = ["PHP", "C", "C#", "Rust", "Java", "JavaScript", "C++", "Go"];
-
-console.log(lenguajes);
-
-invert(lenguajes);
-console.log("Invertido manualmente: ", lenguajes);
