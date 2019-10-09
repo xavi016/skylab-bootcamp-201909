b5aeaadafa28ac60576cac07d6a89033d5c0f4cb
OscarErre <oscarr.rodfer@gmail.com>
2019-10-09 16:23:16 +0200

    sort

--- staff/oscar-rodriguez/array-functions/sort.js
+++ staff/oscar-rodriguez/array-functions/sort.js
@@ -5,13 +5,13 @@ function sort (array) {
     var sorted = false;
     var aux;
 
-    var expresion = arguments.length > 1 ? arguments [1] : (function (a,b) { return b<a? 1:-1 });
-    debugger
+    var expresion = arguments.length > 1 ? arguments [1] : (function (a,b) { return b < a ? 1 : -1 });
+    
     while (!sorted) {
         sorted = true;
         for (var i=0; i<array.length-1; i++){
-            debugger
-            if ((expresion(array[i],array[i+1])>0)) {
+            
+            if ((expresion(array[i].toString().charCodeAt(0),array[i+1].toString().charCodeAt(0))>0)) {
                 aux = array[i+1];
                 array[i+1] = array[i];
                 array[i] = aux;
@@ -20,7 +20,3 @@ function sort (array) {
         }
     }
 }
-
-var arr = [15,4,3,1,8,5];
-sort (arr, function (a,b) { return b - a });
-console.log(arr);
