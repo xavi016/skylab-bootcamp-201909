14af8a4fb7df8e5622861a93615b53bb52ed673a
martapvalls <marpeva14@hotmail.com>
2019-10-10 18:39:30 +0200

    add slice hooray

--- staff/marta-penya/hooray/hooray.js
+++ staff/marta-penya/hooray/hooray.js
@@ -27,9 +27,42 @@ Hooray.prototype.forEach = function(expression) {
  * 
  * @returns {number} The new lenth of the hooray.
  */
+
+
 Hooray.prototype.push = function() { 
 	for (var i = 0; i < arguments.length; i++)
 		this[this.length++] = arguments[i];
 
 	return this.length;
 };
+
+
+/**
+ * Copies a part of the hooray within a new array starting from beginning
+ *  to end (end not included). The original array will not be modified.
+ * 
+ * @param {number} begin Index for starting position 
+ * 
+ * @param {number} end Indez for ending position 
+ * 
+ * @returns {Hooray} New hooray with the extracted values 
+ */
+
+
+
+Hooray.prototype.slice = function(begin, end) {
+	
+    var result = []; 
+
+    begin = begin || 0;
+    begin = begin < 0? this.length + begin : begin;
+    end = end || this.length;
+    end = end < 0? this.length + end : end;
+
+    for (var i = begin; i < end; i++)
+        result[i - begin] = this[i];
+
+    
+    return result;
+}
+
