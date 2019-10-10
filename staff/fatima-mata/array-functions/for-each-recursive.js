714efc52d4c3c0f0dfc0bc33f48b495eb767d17f
Fátima Mata Enrique <fatimamataenrique@MacBook-Pro-de-Fatima.local>
2019-10-10 15:12:01 +0200

    modified files

---
+++ staff/fatima-mata/array-functions/for-each-recursive.js
@@ -0,0 +1,11 @@
+/**
+ * Itearates an array and evaluates an expression on each item.
+ * 
+ * @param {Array} array The array to iterate.
+ * @param {Function} expression The expression to evaluate in each item of the array.
+ */
+(function loop(index) {
+	expression(array[index],index);
+	++index<array.length. && forEachRecursive 
+		if (index<array.length)forEachRecursive(index);
+	}(0)	
--- staff/fatima-mata/array-functions/for-each.js
+++ staff/fatima-mata/array-functions/for-each.js
@@ -1,5 +1,5 @@
 /**
- * Iterates an array and evaluates an expression on each item.
+ * Itearates an array and evaluates an expression on each item.
  * 
  * @param {Array} array The array to iterate.
  * @param {Function} expression The expression to evaluate in each item of the array.
@@ -7,9 +7,10 @@
  * @throws {TypeError} If array is not an array, or expression is not a function.
  */
 function forEach(array, expression) {
-	if (!(array instanceof Array)) throw TypeError(array + ' is not an array')
-	if (typeof expression !== 'function') throw TypeError(expression + ' is not a function')
+	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
+	if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
+
+	//throw Error('🤡');
 	for (var i = 0; i < array.length; i++) 
 		expression(array[i], i, array);
 }
-
