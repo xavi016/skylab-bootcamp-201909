eeb0df751dd76ced5634b69217f0f38e17173a45
Manoli 2013 <websitesbypol@gmail.com>
2019-10-10 18:41:11 +0200

    add hooray slice

--- staff/noli-pascual/hooray/hooray.js
+++ staff/noli-pascual/hooray/hooray.js
@@ -6,7 +6,7 @@ function Hooray() {
 }
 
 /**
- * Iterates the current hooray and evaluates an expression on each item.
+ * FOREACH Iterates the current hooray and evaluates an expression on each item.
  * 
  * @param {Function} expression The expression to evaluate in each item of the hooray.
  * 
@@ -21,7 +21,7 @@ Hooray.prototype.forEach = function(expression) {
 };
 
 /**
- * Pushes a variable number of items into this hooray.
+ * PUSH Pushes a variable number of items into this hooray.
  * 
  * @param {...any} item The item (or items) to push.
  * 
@@ -33,3 +33,34 @@ Hooray.prototype.push = function() {
 
 	return this.length;
 };
+
+
+/**
+ * SLICE method returns a shallow copy of a portion of an hooray into a new hooray object selected from begin to end (end not included) where begin and end represent
+ * the index of items in that hooray. The original hooray will not be modified.
+ * 
+ * @param {Number} begin index, start position.
+ * 
+ * @returns {Number} number end index, last position.
+ * @returns {Hooray} with the portion of the original Hooray.
+ */
+
+Hooray.prototype.slice = function(begin, end) {
+    
+	if (!(array instanceof Array)) throw new TypeError(array + ' is not an array');
+
+    var result = []; // {}; // WTF?
+
+    begin = begin || 0;
+    begin = begin < 0? this.length + begin : begin;
+    end = end || this.length;
+    end = end < 0? this.length + end : end;
+
+    for (var i = begin; i < end; i++)
+        result[i - begin] = this[i];
+
+    //result.length = end - begin; // WTF?
+    
+    return result;
+}
+
---
+++ staff/noli-pascual/hooray/hooray.slice.test.js
@@ -0,0 +1,93 @@
+describe('Hooray.prototype.slice', function() {
+    it('should return a new hooray with the values in the given limits', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice(3, 7);
+        expect(result === numbers).toBe(false);
+        
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expected = [4, 5, 6];
+        for (var i = 0; i < expected.length; i++)
+            expect(result[i]).toBe(expected[i]);
+    });
+   it('should return a new hooray with the values in from the beginning (no ending)', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice(2);
+        expect(result === numbers).toBe(false);
+        // expect(result instanceof Hooray).toBe(true);
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expected = [3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(result[i]).toBe(expected[i]);
+    });
+
+
+    it('should return a new empty hooray (from an empty hooray) from a beginning (no ending)', function() {
+        var numbers = new Hooray();
+        var result = numbers.slice(12);
+        expect(result === numbers).toBe(false);
+        
+        expect(result.length).toBe(0);
+    });
+    it('should return a new hooray with the same values of the original hooray when no beginning and no ending', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice();
+        expect(result === numbers).toBe(false);
+        
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(result[i]).toBe(expected[i]);
+    });
+    it('should return a new hooray with the last values of the original hooray when beginning is negative and no ending', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice(-4);
+        expect(result === numbers).toBe(false);
+       
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expected = [6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(result[i]).toBe(expected[i]);
+    });
+    it('should return a new hooray with values of the original hooray from the beginning and ending, both negatives', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice(-7, -3);
+        expect(result === numbers).toBe(false);
+       
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expected = [3, 4, 5, 6];
+        for (var i = 0; i < expected.length; i++)
+            expect(result[i]).toBe(expected[i]);
+    });
+    it('should return a new hooray with no values when the beginning and ending are both negatives and ending < beginning', function() {
+        var numbers = new Hooray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var result = numbers.slice(-3, -7);
+        expect(result === numbers).toBe(false);
+       
+        var expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (var i = 0; i < expected.length; i++)
+            expect(numbers[i]).toBe(expected[i]);
+        expect(result.length).toBe(0);
+    });
+    // it('should fail on first parameter non-hooray', function() {
+
+    //     var hoorayOne = new Hooray(1,2,3,4);
+    //     expect(function() { slice(); }).toThrowError('undefined is not an hooray');
+    //     expect(function() { slice(true); }).toThrowError('true is not an hooray');
+    //     expect(function() { slice(1); }).toThrowError('1 is not an hooray');
+    //     expect(function() { slice('a'); }).toThrowError('a is not an hooray');
+    // }); 
+});
+
+
+
+
