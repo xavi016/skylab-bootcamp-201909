f8b33dfd82a34e8176e1973f6c331bee93505fc4
martapvalls <marpeva14@hotmail.com>
2019-10-10 17:31:50 +0200

    add new array functions

--- staff/marta-penya/array-functions/for-each.recursive.js
+++ staff/marta-penya/array-functions/for-each.recursive.js
@@ -6,30 +6,20 @@
  * 
  * @throws {TypeError} If array is not an array, or expression is not a function.
  */
-
-
-
 function forEach(array, expression) {
 	if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
 	if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
 
 	//throw Error('🤡');
-	/* for (var i = 0; i < array.length; i++) 
-        expression(array[i], i, array); */
-    
-    (function loop(index) {
-                        
-        expression(array[index], index);
-            
-        ++index < array.lenght && loop(index);
-    })(0);
-            
-            
+	// for (var i = 0; i < array.length; i++) 
+	// 	expression(array[i], i, array);
+
+	// TODO make it work with recursion!
+
+	(function loop(index) {
+		expression(array[index], index);
+
+		//if (++index < array.length) loop(index);
+		++index < array.length && loop(index);
+	})(0);
 }
-        
-
-
-
-
-
-
