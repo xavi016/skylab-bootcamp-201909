d0d1141ee6d18361b948108ddd5b395b11d2642b
OscarErre <oscarr.rodfer@gmail.com>
2019-10-10 17:50:29 +0200

    add some test functions

--- staff/oscar-rodriguez/array-functions/map.js
+++ staff/oscar-rodriguez/array-functions/map.js
@@ -8,8 +8,8 @@
  */
 function map (array, expression) {
 
-    if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
-    if (!(array instanceof Array)) throw TypeError (array + 'is not an array');
+    if (!(array instanceof Array)) throw TypeError (array + ' is not an array');
+    if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');    
     
     var newArray = [];
     for (var i=0; i<array.length; i++) {
--- staff/oscar-rodriguez/array-functions/map.test.js
+++ staff/oscar-rodriguez/array-functions/map.test.js
@@ -1,16 +1,30 @@
 describe ('MAP', function () {
+    it('should apply expression on every array item and save it on a new array', function () {
+        var array = [2,4,6];
+        var expression = function (item) { return item << 2; }
+        var result = map (array, expression);
+
+        expect (array === result).toBe(false);
+
+        for (i=0; i<result.length; i++)
+            expect(result[i]).toBe(array[i]<<2);
+
+    })
 
     it('should throw a TypeError when first parameter is not an array', function () {
-        var array = 2;
+        
+        var expression = function (item) { return item << 2; }
 
-        expect (function () { map(array,function (item) { 
-            return item << 2;
-        })}).toThrow(TypeError,array + 'is not an array');
+        expect (function () { map(2,expression)}).toThrow(TypeError,'2 is not an array');
+        expect (function () { map("a",expression)}).toThrow(TypeError,'a is not an array');
+        expect (function () { map()}).toThrow(TypeError,'undefined is not an array');
+
     })
 
     it('should throw a TypeError when second parameter is not a function', function () {
         var array = [1,2,3,4];
-
-        expect (function () {map(array,"3")}).toThrow(TypeError,expression + ' is not a function');
+        expect (function () { map(array,2)}).toThrow(TypeError,'2 is not a function');
+        expect (function () { map(array,"a")}).toThrow(TypeError,'a is not a function');
+        expect (function () { map(array)}).toThrow(TypeError,'undefined is not a function');
     })
 })
