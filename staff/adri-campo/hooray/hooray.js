d9d500f67bbbe6cdaad545769582472baa6adcbc
Adrián Campo <adribdn7@gmail.com>
2019-10-10 23:12:15 +0200

    function pop added

--- staff/adri-campo/hooray/hooray.js
+++ staff/adri-campo/hooray/hooray.js
@@ -67,3 +67,19 @@ Hooray.prototype.push = function() {
     
     return result;
 }
+
+
+/**
+ * Deletes the last item of an array and returns de deleted item.
+ *  
+ * @returns {any} returns de last item deleted on the array
+ */
+Hooray.prototype.pop = function () {
+    
+    if (this.length===0) return undefined;
+​
+    var result = this[this.length-1];
+    this.length-=1;
+    
+    return result;
+} 
