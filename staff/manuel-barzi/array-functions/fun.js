a20db9dc4b8ee9285bb84444cbbefae244b2a01b
Javier Sevilla <xaviersevilla@gmail.com>
2019-10-10 10:55:40 +0200

    update manu arrays

--- staff/manuel-barzi/array-functions/fun.js
+++
@@ -1,3 +0,0 @@
-function fun(number) {
-    if (number !== 1) throw Error('number is not 1');
-}
--- staff/manuel-barzi/array-functions/fun.test.js
+++
@@ -1,5 +0,0 @@
-describe('fun', function() {
-    it('should fail on not a 1 input', function() {
-        expect(function() { fun(2); }).toThrowError('number is not 1');
-    });
-});
