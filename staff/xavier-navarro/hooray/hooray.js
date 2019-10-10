71698ffcdca18de986edf111251712ca24da1b0c
xavi016 <xaviernavarro_1994@hotmail.com>
2019-10-10 18:40:05 +0200

    add slice

--- staff/xavier-navarro/hooray/hooray.js
+++ staff/xavier-navarro/hooray/hooray.js
@@ -21,6 +21,7 @@ Hooray.prototype.forEach = function(expression) {
 };
 
 /**
+ * PUSH
  * Pushes a variable number of items into this hooray.
  * 
  * @param {...any} item The item (or items) to push.
@@ -32,4 +33,31 @@ Hooray.prototype.push = function() {
 		this[this.length++] = arguments[i];
 
 	return this.length;
+};
+/**
+ * SLICE
+ * Copies a part of the hooray wthin a new hooray starting from the beginning to end
+ *  (end not include). The origal hooray will not be modified.
+ * 
+ * @param {Number} begin Index for starting position.
+ * @param {Number} end Index for the last position.
+ * 
+ * @returns {Hooray} New hooray with the extracted values.
+ */
+Hooray.prototype.slice = function(begin, end) { 
+    //if (typeof end === 'undefined') end = array.length;
+
+    var result = []; // {}; // WTF?
+
+    begin = begin || 0;
+    begin = begin < 0? this.length + begin : begin;
+    end = end || this.length;
+    end = end < 0? this.length + end : end;
+
+    for (var i = begin; i < end; i++)
+        result[i - begin] = this[i];
+
+    //result.length = end - begin; // WTF?
+    
+    return result;
 };
