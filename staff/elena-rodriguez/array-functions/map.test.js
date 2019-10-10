1817aaaebee9e7ad93eb189ecfc6f407b820e4ae
erodrial <elena.erodrial@gmail.com>
2019-10-10 11:31:42 +0200

    add changes

--- staff/elena-rodriguez/array-functions/map.test.js
+++ staff/elena-rodriguez/array-functions/map.test.js
@@ -1,26 +1,34 @@
 describe('map', function () {
-    it('should succeed on correct new array and expression, adding all numbers', function () {
-        var numbers = [1, 2, 3];
-        var result = [];
+    it('should succeed on correct new array and expression, adding two to all numbers', function () {
+        var numbers = [1, 2, 3, 4];
         var addTwo = function (number) { return number + 2};
+        var result = map(numbers, addTwo);
+        expect(result === numbers).toBe(false);
+        expect(result instanceof Array).toBe(true);
 
-        result = map (numbers, addTwo);
-
-        expect(result).toBe([3, 4, 5]);
-    });
-
-
-    it('should succeed on correct new array and expression, doing the square root of each number', function () {
-        var numbers = [1, 2, 3];
-        var result = [];
-        var sqrRoot = function (number) { return Math.sqrt(number); };
-
-        map (numbers, sqrRoot);
-
-        expect(result).toBe([1, 1.4142135623730951, 1.7320508075688772]);
+        var expected = [3, 4, 5, 6];
+        for (var i = 0; i < expected.length; i++)
+        expect(addTwo(numbers[i])).toBe(expected[i]);
     });
 
     
+
+
+    it('should succeed on correct new array and expression, doing the square root of each number', function () {
+        var numbers = [1, 2, 3, 4, 5];
+        var sqrRoot = function (number) { return Math.sqrt(number);};
+        var result = map(numbers, sqrRoot);
+
+        expect(result === numbers).toBe(false);
+        expect(result instanceof Array).toBe(true);
+
+        var expected = [1, 1.4142135623730951, 1.7320508075688772];
+        for (var i = 0; i < expected.length; i++)
+        expect(sqrRoot(numbers[i])).toBe(expected[i]);
+    
+    });
+
+    // HASTA AQUÍ ESTÁ BIENN!!!! :):):) !!!!
 
     it('should fail on undefined array', function () {
         var array; //= [1, 2, 3];
