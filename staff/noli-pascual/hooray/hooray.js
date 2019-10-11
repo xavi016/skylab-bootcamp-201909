08dc5823035ac2c210e44bcb849c52975b266bca
Manoli 2013 <websitesbypol@gmail.com>
2019-10-11 15:46:22 +0200

    update  testing

--- staff/noli-pascual/hooray/hooray.js
+++ staff/noli-pascual/hooray/hooray.js
@@ -47,8 +47,7 @@ Hooray.prototype.push = function() {
 
 Hooray.prototype.slice = function(begin, end) {
     
-	if (!(array instanceof Array)) throw new TypeError(array + ' is not an array');
-
+	
     var result = []; // {}; // WTF?
 
     begin = begin || 0;
@@ -64,3 +63,82 @@ Hooray.prototype.slice = function(begin, end) {
     return result;
 }
 
+/**
+ * SPLICE method changes the contents of an array by
+ * removing or replacing existing elements and/or adding new elements in place.
+ * 
+ * @param {Number} begin index, start position.
+ * 
+ * @returns {Number} number end index, last position.
+ * 
+ * @param {*} items to add to hooray.
+ * 
+ * @returns {Hooray} a hooray containing the deleted elements. If only one element is removed, a hooray of one
+ * element is returned. If no elements are removed, an empty hooray is returned.
+ */
+
+Hooray.prototype.splice = function(start, delCount) { 
+    //if (typeof end === 'undefined') end = array.length;
+	// if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
+	var result = [];
+	var newArray = [];
+	var x = 0;
+	var arrPos = 0;
+	var itemsLen;
+    var items = [];
+    
+	if(typeof delCount === 'undefined'){
+		itemsLen = arguments.length-1;
+		var index = 1;
+	}else{
+		itemsLen = arguments.length-2
+		var index = 2;
+	}
+	for(var y = 0; y<itemsLen; y++){
+		items[y] = arguments[index];
+		index++;
+	}
+	var finalLength = this.length-delCount+itemsLen;
+	
+	// Deleted elements
+	for (var i = 0; i < delCount; i++) {
+		result[i] = this[i+start];
+	}
+	while (x < finalLength) {
+		if(x === start){
+			for(var y =0; y<delCount; y++){
+				arrPos++
+			}
+			for(var y = 0; y<itemsLen; y++){
+				newArray[x] = items[y]
+				x++
+			}
+		}else{
+			newArray[x] = this[arrPos] 
+			x++
+			arrPos++
+		}
+	}
+	this.length = newArray.length
+	for(var i = 0; i<this.length;i++){
+		this[i] = newArray[i]
+	}
+​
+    return this;
+};
+
+/**
+ * The map() method creates a new hooray with the results of calling a
+ * provided function on every element in the calling hooray.
+ * 
+ * @param {Function} function that provides what to do to the elements of the hooray.
+ * 
+ * @returns {Hooray} hooray with the elements of original hooray but modified.
+ * 
+ 
+ */
+
+ Hooray.prototype.map = function(expression) {
+     //ToDo
+
+ }
