02dbba103a0ddb6aa555de47591ed4ea968db324
Jose Rodriguez <jechto610@gmail.com>
2019-10-11 10:07:55 +0200

    deleted array-functions wrong files on branch feature/hooray

--- staff/jose-rodriguez/array-functions/findIndex.js
+++
@@ -1,9 +0,0 @@
-
-
-function findIndex(array, expression){
-    for (var i=0; i<array.length; i++){ 
-        if (expression(array[i])){ 
-            return i;
-        } 
-    } return -1;
-}
--- staff/jose-rodriguez/array-functions/pop-demo.js
+++
@@ -1,6 +0,0 @@
-console.log('DEMO pop');
-
-var array = [1,2,3,4];
-console.log('should return the last element of the array');
-console.log(pop(array)); // 4
-console.log(array);
