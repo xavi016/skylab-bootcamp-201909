272cf02823adb201cdf9fa48074468f2497827d5
xavi016 <xaviernavarro_1994@hotmail.com>
2019-10-12 10:20:55 +0200

    add fill,every,splice

--- staff/xavier-navarro/hooray/hooray.push.spec.js
+++ staff/xavier-navarro/hooray/hooray.push.spec.js
@@ -5,20 +5,14 @@ describe('Hooray.prototype.push', function () {
         expect(hooray.push('d')).toBe(4);
         expect(hooray.length).toBe(4);
         expect(hooray[hooray.length - 1]).toBe('d');
-
-        var chars = ['a', 'b', 'c', 'd'];
-        for (var i = 0; i < chars.length; i++)
-            expect(hooray[i]).toBe(chars[i]);
+        expect(hooray).toContain('a', 'b', 'c', 'd');
     });
 
     it('should push multiple items', function () {
         var hooray = new Hooray('a', 'b', 'c');
 
         expect(hooray.push('d', 'e', 'f', 'g', 'h', 'i', 'j', 'k')).toBe(11);
-        expect(hooray.length).toBe(11);
-
-        var chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
-        for (var i = 0; i < hooray.length; i++)
-            expect(hooray[i]).toBe(chars[i]);
+        expect(hooray.length).toBe(11);  
+        expect(hooray).toContain('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k');
     });
 });
