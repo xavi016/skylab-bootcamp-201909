652e3460da562da3db0b13d1f308f83fcda0066c
martapvalls <marpeva14@hotmail.com>
2019-10-12 10:20:24 +0200

    splice fill and every added

--- staff/marta-penya/hooray/hooray.js
+++ staff/marta-penya/hooray/hooray.js
@@ -69,70 +69,152 @@ Hooray.prototype.slice = function(begin, end) {
 
 
 /**
- * Changes the contents of an hooray by removing or replacing existing elements and/or adding new elements in place.
+ * SPLICE
+ * Changes the contents of an hooray by removing or replacing
+ *  existing elements and/or adding new elements in place.
  * 
- * @param {number} start Index for starting position 
+ * @param {Number} start Index for starting position.
+ * @param {Number} delCount Index for the last position.
+ * @param {*} items Index for the last position.
  * 
- * @param {number} delCount Indez for ending position 
- * 
- * @param {*} item The elements to add to the array 
- * 
- * @returns {Hooray} An hooray containing the deleted elements. If only one element is removed, 
- * an hooray of one element is returned. If no elements are removed, an empty hooray is returned.
+ * @returns {Hooray} An hooray containing the deleted elements.
+ * If only one element is removed, an hooray of one element is returned.
+ *  If no elements are removed, an empty hooray is returned.
  */
 
 
 Hooray.prototype.splice = function(start, delCount) { 
     //if (typeof end === 'undefined') end = array.length;
 	// if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
-	var result = new Hooray;
-	var newHooray = [];
-	var x = 0;
-	var arrPos = 0;
-	var itemsLen;
-    var items = [];
-    
+	var result = new Hooray; // nuevo hooray con los elementos eliminados
+	var newHooray = []; // introducir nuevos valores del hooray original
+	var x = 0; // iniciar el bucle y recorrer el hooray
+	var arrPos = 0; // indice para colocar los elementos correctamente
+	var itemsLen; // longitud de los items a introducir
+	var items = []; // items a introducir
+	// debugger
 
-	if(typeof delCount === 'undefined'){
+
+	if(delCount === 'undefined'){ // si no hay elementos a eliminar
 		itemsLen = arguments.length-1;
 		var index = 1;
-	}else{
+	}else if(arguments.length === 2 && start !== 'undefined' && start >= 0 && delCount !== 'undefined'){ // si no hay items a introducir
+		var index = 0;
+		itemsLen = 0;
+	}else if(arguments.length === 1){ // si solo se introduce un parámetro numérico 
+		delCount = this.length-start;
+		itemsLen = this.length-start;
+	} else if(start < 0){ // si start es negativo
+		itemsLen = arguments.lenght - delCount + start;
+		start = this.length+start;
+	}else{ // si introducimos todos los parametros 
 		itemsLen = arguments.length-2
 		var index = 2;
 	}
-	for(var y = 0; y<itemsLen; y++){
-		items[y] = arguments[index];
-		index++;
-    }
-    
-
-	var finalLength = this.length-delCount+itemsLen;
 	
-	// Deleted elements
+	// extracción de items de arguments y guardarlos en var items
+	if(itemsLen !== 0){
+		var finalLength = this.length-delCount;
+		// ADD ['Jan','March']
+		for(var y = 0; y<itemsLen; y++){
+			items[y] = arguments[index];
+			index++;
+		}
+	}else {
+		var finalLength = this.length-delCount+itemsLen;
+	}
+	// guardar items eliminados en el hooray results
 	for (var i = 0; i < delCount; i++) {
 		result[i] = this[i+start];
+		result.length++;
 	}
+	//bucle para colocar los items correctamente en el new hooray
+	var lol = false;
 	while (x < finalLength) {
-		if(x === start){
+		if(x === start && lol === false){
 			for(var y =0; y<delCount; y++){
 				arrPos++
 			}
-			for(var y = 0; y<itemsLen; y++){
-				newHooray[x] = items[y]
-				x++
+			if(itemsLen !== 0){
+				for(var y = 0; y<itemsLen; y++){
+					newHooray[x] = items[y]
+					x++
+				}
 			}
+			lol = true
 		}else{
 			newHooray[x] = this[arrPos] 
 			x++
 			arrPos++
 		}
 	}
-	this.length = newHooray.length
+	// arreglar lenght del hooray original y añadir items(si hay)
+	// debugger
 	for(var i = 0; i<this.length;i++){
 		this[i] = newHooray[i]
-    }
-    
+	}
+
+	for(var x = delCount; x>0; x--){
+		delete this[this.length-1];
+		this.length--
+	}
+	// debugger
+	console.log(result);
+	console.log(this);
+
     return result;
 };
 
 
+/**
+ * Fill the hooray to iterate with the item that you sent
+ * 
+ * @param {Hooray} hooray The hooray to iterate.
+ * @param {*} newItem Character by which we will replace items from hooray
+ * 
+ */
+Hooray.prototype.fill = function(newItem){
+	var start = 0;
+	var end = this.length;
+	
+	switch(arguments.length){
+	  case 2:
+		start = arguments[1];
+		break;
+	  case 3:
+		start = arguments[1];
+		end = arguments[2];
+		break;
+	  default:
+		  if (arguments.length < 1 ) newItem = undefined;
+		break;
+	}
+
+	for(var i = start; i < end; i++){
+		this[i] = newItem;
+	  }  
+	  
+  };
+
+
+   /**
+ * Checks if all the elements in the array pass the condition implemented by the given function
+ * 
+ * @param {Array} array The array to evaluate elements to the condition given 
+ * 
+ * @param {Function} expression The expression to evaluate in each item of the array.
+ * 
+ * @returns {boolean} returns true if all the elements in the array pass the condition; otherwise, false.
+ * 
+ */
+
+
+
+Hooray.prototype.every = function (expression) {
+	if (typeof expression !== 'function') throw TypeError(expression + ' is not a function');
+	
+    for (let i = 0; i < this.length; i++) {
+        if (!expression(this[i])) return false;
+    }
+    return true;
+};
