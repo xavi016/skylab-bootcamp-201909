fa4c5a5aca5a4b226c554456830a7b9292881702
Diana <d7@hotmail.es>
2019-10-13 18:48:42 +0200

    merge random with shuffle

--- staff/diana-moreno/array-functions/shuffle.js
+++ staff/diana-moreno/array-functions/shuffle.js
@@ -13,3 +13,24 @@ function shuffle(array) {
   };
   return result;
 };
+
+/*
+// another version
+
+function shuffle(array) {
+  let newArray = [];
+  let copyArray = [];
+
+  for(let i = 0; i < array.length; i++) {
+    copyArray[i] = array[i];
+  };
+  //let copyArray = [...array] // en ES6 usamos spread operator, en ES5 usamos for loop
+
+  for(let i = 0; i < array.length; i++)  {
+    let randomIndex = Math.floor(Math.random() * copyArray.length)
+    newArray[i] = copyArray[randomIndex]
+    copyArray.splice(randomIndex, 1)
+  };
+  return newArray;
+};
+*/
