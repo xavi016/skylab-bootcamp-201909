7ac06ffc6af0c6ad6f29de8622cd2e5bdd0c4890
erodrial <elena.erodrial@gmail.com>
2019-10-13 19:24:18 +0200

    save changes

--- staff/elena-rodriguez/array-functions/concat.js
+++ staff/elena-rodriguez/array-functions/concat.js
@@ -7,22 +7,33 @@
  * @returns {array} array with the diferent arrays or elements concatenated.
  * */
 
-function concat(array) {
-    var aux = array;
-    var cont = array.length;
-    for (let i = 1; i < arguments.length; i++) {
-        if (arguments[i] instanceof Array) {
-            for (let j = 0; j < arguments[i].length; j++) {
-                aux[cont] = arguments[i][j];
-                cont++;
-            }
-        }
-        else {
-        aux[cont] = arguments [i]; 
-        cont++;
-        }
+
+
+function concat(array) { 	
+    if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
+
+    var newArray = []; 
+
+    var iterat = 0;
+    for (var y = 0; y < arguments[0].length; y++){
+        newArray[iterat] = arguments[0][y];
+        iterat += 1;
     }
-    return aux;
-  }
+
+    for (var i = 1; i < arguments.length; i++) {
+        if (arguments[i] instanceof Array) {
+            for (var j = 0; j < arguments[i].length; j++) {
+                newArray[iterat] =  arguments[i][j];  
+                iterat += 1;
+            }           
+        } else {
+            newArray[iterat] = arguments[i];
+            iterat += 1;
+        } 
+    }       
+    return newArray;
+    
+}
+
 
   
