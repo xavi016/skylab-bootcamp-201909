5c464bd93f1cb8ee21570083223430b00535dd4b
Javier Sevilla <xaviersevilla@gmail.com>
2019-10-13 16:06:58 +0200

    update arrays functions jasmine

--- staff/javier-sevilla/array-functions/some.js
+++ staff/javier-sevilla/array-functions/some.js
@@ -1,18 +1,18 @@
 /**
- * check if all array elements pass the test implemented by the expression
+ * check if one array elements pass the test implemented by the expression
  * 
  * @param {*} array array The array to iterate.
  * @param {*} expression The expression to evaluate in each item of the array.
  * 
- * retunr {bolean} return true or false
+ * @return{result} return true or false
  */
-function every(array, expression) { 	
+function some(array, expression) { 	
     if (!(array instanceof Array)) throw TypeError(array + ' is not an array');
     if (typeof expression !== 'function') throw TypeError(expression + ' is not a function')
-    var boleana;
+    var boleana;;
 	for (var i = 0; i < array.length; i++) {    
         boleana = expression(array[i])
-        if (!(boleana)) return false;        
+        if (boleana) return true;        
     }
-    return true;
+    return false;
 }
