7e659ad92997e47a37a5dcac95cf2c8d9fb9cdfb
erodrial <elena.erodrial@gmail.com>
2019-10-14 12:58:07 +0200

    save changes

--- staff/elena-rodriguez/array-functions/unshift.demo.js
+++
@@ -1,8 +0,0 @@
-console.log('DEMO unshift');
-
-
-var array = [1, 2, 3, 4];
-var items = ['hola','mundo'];
-
-console.log("should return ['hola','mundo',1,2,3,4]");
-console.log(unshift(array, items));
