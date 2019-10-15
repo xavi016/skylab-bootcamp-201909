6c63ad84a05d93ec4d7ba07f183109d5a46b9161
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-10-15 17:56:11 +0200

    modify         duck-app/index.html         duck-app/utils/article.sass         duck-app/utils/main.js         duck-app/utils/search__main.sass         duck-app/utils/style.css         duck-app/utils/style.sass
    Create
            duck-app/utils/header.sass
            duck-app/utils/result.sass
            duck-app/utils/search.sass

--- staff/martin-garcia/duck-app/utils/main.js
+++ staff/martin-garcia/duck-app/utils/main.js
@@ -3,6 +3,8 @@ var searchElement = document.getElementsByClassName('search')[0];
 var resultElement = document.getElementsByClassName('result')[0];
 resultElement.hidden = true;
 
+search();
+
 
 function search(e) {
     var textToFind = myForm[0][0].value;
@@ -13,13 +15,15 @@ function search(e) {
 
         if (this.readyState == 4 && this.status == 201) {
             var ducks = JSON.parse(xhr.responseText);
+
             constructSearchHtml(ducks);
         }
     };
 
     xhr.send();
 
-    e.preventDefault();
+    if (typeof e === undefined)
+        e.preventDefault();
 }
 
 
@@ -33,7 +37,8 @@ function constructSearchHtml(ducks) {
         var article = document.createElement('article');
         article.className = 'main__article article';
 
-        article.setAttribute('onclick', "constructResultHtml('" + duck.title + "', '" + duck.imageUrl + "','" + duck.price + "')");
+        article.setAttribute('onclick', "constructResultHtml('" + duck.id + "')");
+        //article.addEventListener('click', constructResultHtml(duck.id));
 
         var title = document.createElement('p');
         title.className = 'article__title';
@@ -54,33 +59,48 @@ function constructSearchHtml(ducks) {
 
 
 
-function constructResultHtml(titleText, imageText, priceText) {
+function constructResultHtml(id) {
     searchElement.hidden = true;
     resultElement.hidden = false;
 
-    var container = document.getElementsByClassName('result__main');
-    container = container[0];
-    container.innerHTML = "";
+    var xhr = new XMLHttpRequest;
 
-    var article = document.createElement('article');
-    article.className = 'main__article article';
+    xhr.open('GET', 'https://duckling-api.herokuapp.com/api/ducks/' + id);
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 201) {
+            var duck = JSON.parse(xhr.responseText);
+            var container = document.getElementsByClassName('result__main');
+            container = container[0];
+            container.innerHTML = "";
+
+            var article = document.createElement('article');
+            article.className = 'main__article article';
 
 
-    var title = document.createElement('p');
-    title.className = 'article__title';
-    title.innerText = titleText;
+            var title = document.createElement('p');
+            title.className = 'article__title';
+            title.innerText = duck.title;
 
-    var image = document.createElement('img');
-    image.className = 'article__image';
-    image.src = imageText;
+            var image = document.createElement('img');
+            image.className = 'article__image';
+            image.src = duck.imageUrl;
 
-    var price = document.createElement('p');
-    price.className = 'article__price';
-    price.innerText = priceText;
+            var price = document.createElement('p');
+            price.className = 'article__price';
+            price.innerText = duck.price;
 
-    article.append(title, image, price);
-    container.append(article);
 
+            var description = document.createElement('p');
+            description.className = 'article__description';
+            description.innerText = duck.description;
+
+            article.append(title, image, description, price);
+            container.append(article);
+        }
+    };
+
+    xhr.send();
 }
 
 
