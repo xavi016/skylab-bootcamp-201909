c74c43333e39011c7920fc6cfd2876c9502bb9e3
Fátima Mata Enrique <fatimamataenrique@MacBook-Pro-de-Fatima.local>
2019-10-15 09:16:21 +0200

    modified files

--- staff/fatima-mata/array-functions/filter.demo.js
+++
@@ -1,7 +0,0 @@
-console.log('DEMO filter');
-
-var array = [1, 2, 3];
-
-console.log('should Delete the last element of the array and return it.');
-console.log(filter(array)); // 3
-console.log(array); // [1, 2]
