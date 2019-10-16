f6ee39785db90ea75473b89fca752d885c31182c
martapvalls <marpeva14@hotmail.com>
2019-10-16 14:17:17 +0200

    adding tablet and pc view

--- staff/marta-penya/duck-app/main.js
+++ staff/marta-penya/duck-app/main.js
@@ -1,7 +1,86 @@
+function shuffle() {
+        
+    var result = [];
+
+    for (var i = 0; i < array.length; i++) result[i] = array[i];
+
+    for (var i = 0; i < result.length; i++) {
+        var random = Math.floor(Math.random() * result.length); // 0 <> length-1
+
+        var value = result[i];
+
+        result[i] = result[random];
+
+        result[random] = value;
+    }
+
+    return result;
+}
+
+
 var button = document.getElementsByClassName('search__form');
 var item = document.getElementsByClassName('ducks');
 var input = document.getElementById('search__formitem');
 var result = document.getElementsByClassName('result');
+
+var xhr = new XMLHttpRequest;
+
+xhr.open('GET', 'https://duckling-api.herokuapp.com/api/search');
+
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 201) {
+        var ducks = JSON.parse(xhr.responseText);
+        ducks = ducks.shuffle().splice(0, 3);
+
+        var ul = document.createElement('ul');
+        ul.classList.add('duck-list');
+
+        item[0].append(ul);
+
+       
+
+        ducks.forEach(function (duck) {
+            var link = document.createElement('a');
+            link.classList.add('duck-list__link');
+            link.addEventListener('click', function (e) {showItem(duck.id)} );
+
+            var li = document.createElement('li');
+            li.classList.add('duck-list__duck');
+
+            var img = document.createElement('img');
+            img.classList.add('duck-list__image');
+            img.src = duck.imageUrl;
+            
+            var h2 = document.createElement('h2');
+            var text = document.createTextNode(duck.title);
+            h2.classList.add('duck-list__title');
+            h2.appendChild(text);
+                
+            
+            var price = document.createElement('p');
+            var pric = document.createTextNode(duck.price);
+            price.classList.add('duck-list__price');
+            price.appendChild(pric);
+            
+
+
+            link.append(h2);
+            link.append(img);
+            link.append(price);
+
+            li.append(link);
+
+            ul.append(li);
+        });
+    } 
+};
+
+xhr.send();
+
+
+
+
+
 
 button[0].addEventListener('submit', function (e) {
     e.preventDefault();
@@ -56,7 +135,7 @@ button[0].addEventListener('submit', function (e) {
 
                 ul.append(li);
             });
-        }
+        } 
     };
 
     xhr.send();
