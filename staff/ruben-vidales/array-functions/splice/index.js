71e8c4c1e6b6d67a33e873a1a8f2579a1abfbfc9
Ruben Vidales <rubenvj85@gmail.com>
2019-10-16 23:14:31 +0200

    last corrections

--- staff/ruben-vidales/array-functions/splice/index.js
+++ staff/ruben-vidales/array-functions/splice/index.js
@@ -1,3 +1,38 @@
-function splice (array, start, numberOfOcurrences) {
 
+function splice(array, start) {
+
+    if (!(array instanceof Array)) throw TypeError(array + 'is not an array');
+
+    var deleteCount = arguments[2] || 0;
+    if (start < 0) start = (array.length) + start;
+
+
+    var deleted = [];
+    if (start >= array.length) start = array.length - 1;
+    if (start + deleteCount > array.length) deleteCount = array.length - start;
+
+    if (deleteCount) {
+
+        for (var i = start; i < start + deleteCount; i++) {
+            deleted[deleted.length] = array[i];
+        }
+
+        for (var i = start; i < array.length; i++) {
+            if ((i + deleteCount) < array.length)
+                array[i] = array[i + deleteCount];
+        }
+
+        array.length -= deleteCount;
+    }
+
+    if (arguments.length > 3) {
+        for (var i = array.length - 1; i >= start; i--) {
+            array[i + (arguments.length - 3)] = array[i];
+        }
+        for (i = 0; i < (arguments.length - 3); i++) {
+            array[start + i] = arguments[i + 3];
+        }
+    }
+
+    return deleted;
 }
