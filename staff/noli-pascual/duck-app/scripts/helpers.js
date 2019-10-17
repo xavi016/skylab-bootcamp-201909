9fbd6a5791048555c08e1400af2c8eab32af88b4
Manoli 2013 <websitesbypol@gmail.com>
2019-10-17 18:41:05 +0200

    erase files

--- staff/noli-pascual/duck-app/scripts/helpers.js
+++
@@ -1,9 +0,0 @@
-function call(method, url, callback) {
-    fetch(method, url, function (response) {
-        if (response.readyState == 4 && response.status == 201) {
-            var results = JSON.parse(response.responseText);
-
-            callback(results);
-        }
-    });
-}
