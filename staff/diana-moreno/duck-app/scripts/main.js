71f9ce36175dc1dae47505c76150f171ff68db2d
Diana <d7@hotmail.es>
2019-10-17 10:08:30 +0200

    add random ducks to initial screen. -Iteration 3 is working-

--- staff/diana-moreno/duck-app/scripts/main.js
+++ staff/diana-moreno/duck-app/scripts/main.js
@@ -1,6 +1,15 @@
 var form = document.getElementsByClassName('form')[0];
 var duckList = document.getElementsByClassName('duck__list')[0];
 
+listInitialRandomDucks();
+
+function listInitialRandomDucks() {
+  searchDucks('', function (ducks) {
+    ducks = ducks.shuffle().splice(0, 6);
+    paintResults(ducks);
+  })
+};
+
 var search = new Search(form);
 search.onSubmit(listSearchResults);
 
