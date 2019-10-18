3a0043de18002af2f03654a9fd6a99246c79f755
Manuel Barzi <manuelbarzi@gmail.com>
2019-10-18 18:41:54 +0200

    boost duck app

---
+++ staff/manuel-barzi/duck-app/7/scripts/helpers/call.js
@@ -0,0 +1,13 @@
+function call(method, url, body, callback) {
+    let headers = {}
+
+    if (body) headers['Content-Type'] = 'application/json;charset=UTF-8'
+
+    fetch(method, url, headers, body, function (response) {
+        if (response.readyState == 4) {
+            var result = JSON.parse(response.responseText);
+
+            callback(result);
+        }
+    });
+}
---
+++ staff/manuel-barzi/duck-app/7/scripts/logic/retrieve-duck/index.js
@@ -0,0 +1,8 @@
+function retrieveDuck(id, callback) {
+    if (typeof id !== 'string') throw new TypeError(id +  ' is not a string');
+    if (typeof callback !== 'function') throw new TypeError(callback +  ' is not a function');
+
+    call('GET', 'https://duckling-api.herokuapp.com/api/ducks/' + id, undefined, function (result) {
+        result.error ? callback(new Error(result.error)) : callback(undefined, result);
+    });
+}
