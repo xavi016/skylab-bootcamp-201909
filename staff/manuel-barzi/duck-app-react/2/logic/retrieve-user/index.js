1966b47cc9efcbd9af02471eef1213e036bb7210
Manuel Barzi <manuelbarzi@gmail.com>
2019-10-24 14:07:39 +0200

    boost duck-app-react, add toggle fav duck logic

--- staff/manuel-barzi/duck-app-react/2/logic/retrieve-user/index.js
+++ staff/manuel-barzi/duck-app-react/2/logic/retrieve-user/index.js
@@ -3,7 +3,7 @@ function retrieveUser(id, token, callback) {
     if (!id.trim().length) throw new ContentError('id is empty or blank')
     if (typeof token !== 'string') throw new TypeError(token + ' is not a string')
     if (!token.trim().length) throw new ContentError('token is empty or blank')
-    if (typeof callback !== 'function') throw new TypeError(callback +  ' is not a function');
+    if (typeof callback !== 'function') throw new TypeError(callback +  ' is not a function')
 
     call('GET', token, `https://skylabcoders.herokuapp.com/api/user/${id}`, undefined, result => {
         result.error ? callback(new Error(result.error)) : callback(undefined, result.data)
