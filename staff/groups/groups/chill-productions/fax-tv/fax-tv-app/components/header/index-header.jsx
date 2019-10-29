0c8ad1feea4ed7bd3126da9517a9e7c639adcb23
Aitor Parra <aitor1979@protonmail.com>
2019-10-29 17:15:07 +0100

    add header, search

---
+++ staff/groups/groups/chill-productions/fax-tv/fax-tv-app/components/header/index-header.jsx
@@ -0,0 +1,32 @@
+function Header({ onLogout }) {
+
+    return <header className="header">
+
+        <nav className="main__nav nav" role="navigations">
+            <div className="nav__logo"><p className="company-name">faxTV</p></div>
+            <Search />
+           
+            
+            <ul className="nav__menu menu">
+                <li className="menu__item" onclick="">
+                    <i className="fas fa-user-circle"></i>
+                    <i className="fas fa-ellipsis-v"></i>
+                    <ul className="menu_sub">
+                        <li className="menu_sub__item">Favorites</li>
+                        <li className="menu_sub__item">My rating</li>
+                        <li className="menu_sub__item">
+                            <a href="" onClick={event => {
+                                event.preventDefault()
+
+                                onLogout()
+                            }}>
+                                <i className="fas fa-power-off"></i> Log Out
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+}
