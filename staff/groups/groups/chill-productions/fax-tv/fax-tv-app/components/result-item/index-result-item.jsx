3ca82fe6274f2a7a7eccabb0272c06deb1409511
xavi016 <xaviernavarro_1994@hotmail.com>
2019-11-01 09:45:18 +0100

    modify home css

--- staff/groups/groups/chill-productions/fax-tv/fax-tv-app/components/result-item/index-result-item.jsx
+++ staff/groups/groups/chill-productions/fax-tv/fax-tv-app/components/result-item/index-result-item.jsx
@@ -1,6 +1,6 @@
 function ResultItem({ item: { id, title, poster_path, year, isFav }, onClick, onFav }) {
     return <li className="results__item item">
-                <a href="#" className="item" onClick={event => {
+                <a href="#" className="item__link" onClick={event => {
                             event.preventDefault()
                             
                             if(event.target.classList.contains("mov__fav")){
@@ -11,9 +11,10 @@ function ResultItem({ item: { id, title, poster_path, year, isFav }, onClick, on
 
                         }}>
                     <img src={poster_path} className="item__poster" alt="Film"/>
-                    <p className="item__title">{title}</p>
+                    {/* <p className="item__title">{title}</p> */}
                     {/* <p className="item__year">{year}</p> */}
-                    <span className="mov__fav">{isFav ? "❤" : "💥"}</span>
+                    <span className="mov__fav">{isFav ? "❤" : "💩"}</span>
+                    <p className="item__details">View more details</p>
                 </a>
             </li>
 }
