2ff2e913e71de2e6aba15b8767ed5ac6229e7b21
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-04 17:06:27 +0100

    upgrade

---
+++ staff/adrian-zhu/duck-app/scripts/components/detail.js
@@ -0,0 +1,29 @@
+class Detail extends Component {
+    constructor(container) {
+        super(container)
+    }
+    
+    onBack(){};
+
+    render(item) {
+
+        var title = this.container.getElementsByClassName('detail__title')[0];
+        title.innerText = item.title;
+        
+        var image = this.container.getElementsByClassName('detail__image')[0];
+        image.src = item.imageUrl;
+    
+        var description = this.container.getElementsByClassName('detail__description')[0];
+        description.innerText = item.description;
+    
+        var store = this.container.getElementsByClassName('detail__store')[0];
+        store.href = item.link;
+    
+        var price = this.container.getElementsByClassName('detail__price')[0];
+        price.innerText = item.price;
+    
+        var back = this.container.getElementsByClassName('detail__back')[0];
+        back.addEventListener('click', this.onBack);
+
+    }
+}
