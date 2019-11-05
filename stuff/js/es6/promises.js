0e1b806e821e217d70460095ccf02c8a20e793f6
Manuel Barzi <manuelbarzi@gmail.com>
2019-11-05 16:38:17 +0100

    boost promises demos

--- stuff/js/es6/promises.js
+++ stuff/js/es6/promises.js
@@ -1,4 +1,4 @@
-// 0
+// DEMO synchronous operations
 
 new Promise((resolve, reject) => {
 	resolve(10)
@@ -11,7 +11,24 @@ new Promise((resolve, reject) => {
 	.then(res => console.log(res))
 	.catch(error => console.error(2, error.message))
 
-// 1
+// DEMO asynchronous operations
+
+new Promise((resolve, reject) => {
+	resolve(10)
+})
+	.then(res => res * 10)
+	.then(res => { console.log(res); return res })
+	//.catch(error => console.error(1, error.message))
+	//.then(res => { throw Error(res) })
+	.then(res => new Promise((resolve, reject) => setTimeout(() => resolve(res * 5), 5000)))
+	.then(res => res + 10)
+	.then(res => { console.log(res); return res })
+	.then(res => new Promise((resolve, reject) => setTimeout(() => resolve(res + 2000), 3000)))
+	.then(res => res + 10)
+	.then(res => console.log(res))
+	//.catch(error => console.error(2, error.message))
+
+// DEMO parallel, no order
 
 fetch('http://duckling-api.herokuapp.com/api/search?q=green') // Fetch API
 	.then(res => res.json())
@@ -20,3 +37,12 @@ fetch('http://duckling-api.herokuapp.com/api/search?q=green') // Fetch API
 									.then(res => res.json())
 									.then(duck => console.log(duck)))
 	})
+
+// DEMO parallel, with order
+
+fetch('http://duckling-api.herokuapp.com/api/search?q=green') // Fetch API
+	.then(res => res.json())
+	.then(ducks => Promise.all(ducks.map(({id}) => fetch(`http://duckling-api.herokuapp.com/api/ducks/${id}`)
+									.then(res => res.json()))))
+	.then(ducks => ducks.forEach(duck => console.log(duck)))
+
