ec1c11173808e094630513012e4a28c6d1e7e7d4
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-05 17:35:57 +0100

    upgrade

--- staff/adrian-zhu/learnyoucode/08- HTTP Collect/httpCollect.js
+++ staff/adrian-zhu/learnyoucode/08- HTTP Collect/httpCollect.js
@@ -1,24 +1,24 @@
-// const http = require('http');
-// http.get(process.argv[2], (response) => {
-//     const result = '';
-//     response.setEncoding('utf8');
-//     response.on('data',(data) => {
-//         result += data;
-//     });
-//     response.on('end', () => {
-//         console.log(result.length);
-//         console.log(result);
-//     });
-// });
+const http = require('http');
+http.get(process.argv[2], (response) => {
+    let result = '';
+    response.setEncoding('utf8');
+    response.on('data',(data) => {
+        result += data;
+    });
+    response.on('end', () => {
+        console.log(result.length);
+        console.log(result);
+    });
+});
 
-const http = require('http')
-const bl = require('bl')
+// const http = require('http')
+// const bl = require('bl')
 
-http.get(process.argv[2], function (response) {
-    response.pipe(bl(function (err, data) {
-        if (err) return console.error(err)
-        data = data.toString()
-        console.log(data.length)
-        console.log(data)
-    }))
-})
+// http.get(process.argv[2], function (response) {
+//     response.pipe(bl(function (err, data) {
+//         if (err) return console.error(err)
+//         data = data.toString()
+//         console.log(data.length)
+//         console.log(data)
+//     }))
+// })
