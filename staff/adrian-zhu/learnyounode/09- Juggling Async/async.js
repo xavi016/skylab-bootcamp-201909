04de6736444ba3cde5f54e7e60c49f8e8b5e2136
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-06 15:10:32 +0100

    up

--- staff/adrian-zhu/learnyounode/09- Juggling Async/async.js
+++ staff/adrian-zhu/learnyounode/09- Juggling Async/async.js
@@ -1,50 +1,22 @@
 const http = require('http')
+const bl = require('bl') 
 
-// $ node . http://google.com http://google.es http://google.it
-const { argv: [, , ...urls] } = process
+const urls = process.argv.splice(2).reverse()
 
-const outputs = []
-let counter = 0
-
-urls.forEach((url, index) => { 
-    const request = http.get(url, response => { 
-        response.setEncoding('utf8')
-
-        response.on('error', error => { throw error })
-
-        let content = ''
-
-        response.on('data', chunk => content += chunk)
-
-        response.on('end', () => {
-            outputs[index] = content
-
-            ++counter === urls.length && outputs.forEach(output => console.log(output))
-        })
+let total = []
+function httpGet(turn){
+    if(turn<0){
+        console.log(total.join('\n'))
+        return
+    }
+    http.get(urls[turn],function(resp){
+        resp.pipe(bl(function(err,data){
+            if(err){
+                console.error(`error:${err.message}`)
+            }
+            total.push( data+'' )
+            httpGet(--turn)
+        }))
     })
-
-    request.on('error', error => { throw error })
-})
-
-// const http = require('http')
-// const bl = require('bl') // container of buffer => es un formato que parece a objecto en node.js 
-
-// const urls = process.argv.splice(2).reverse()
-
-// let total = []
-// function httpGet(turn){
-//     if(turn<0){
-//         console.log(total.join('\n'))
-//         return
-//     }
-//     http.get(urls[turn],function(resp){
-//         resp.pipe(bl(function(err,data){
-//             if(err){
-//                 console.error(`error:${err.message}`)
-//             }
-//             total.push( data+'' )
-//             httpGet(--turn)
-//         }))
-//     })
-// }
-// httpGet(2)
+}
+httpGet(2)
