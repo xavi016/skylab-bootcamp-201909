27ef3cf4e7f22196021ee0735e6427cda6f9d285
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-06 11:54:10 +0100

    upgrade

--- staff/adrian-zhu/learnyounode/09- Juggling Async/async.js
+++ staff/adrian-zhu/learnyounode/09- Juggling Async/async.js
@@ -1,22 +1,50 @@
 const http = require('http')
-const bl = require('bl') // container of buffer => es un formato que parece a objecto en node.js 
 
-const urls = process.argv.splice(2).reverse()
+// $ node . http://google.com http://google.es http://google.it
+const { argv: [, , ...urls] } = process
 
-let total = []
-function httpGet(turn){
-    if(turn<0){
-        console.log(total.join('\n'))
-        return
-    }
-    http.get(urls[turn],function(resp){
-        resp.pipe(bl(function(err,data){
-            if(err){
-                console.error(`error:${err.message}`)
-            }
-            total.push( data+'' )
-            httpGet(--turn)
-        }))
+const outputs = []
+let counter = 0
+
+urls.forEach((url, index) => { 
+    const request = http.get(url, response => { 
+        response.setEncoding('utf8')
+
+        response.on('error', error => { throw error })
+
+        let content = ''
+
+        response.on('data', chunk => content += chunk)
+
+        response.on('end', () => {
+            outputs[index] = content
+
+            ++counter === urls.length && outputs.forEach(output => console.log(output))
+        })
     })
-}
-httpGet(2)
+
+    request.on('error', error => { throw error })
+})
+
+// const http = require('http')
+// const bl = require('bl') // container of buffer => es un formato que parece a objecto en node.js 
+
+// const urls = process.argv.splice(2).reverse()
+
+// let total = []
+// function httpGet(turn){
+//     if(turn<0){
+//         console.log(total.join('\n'))
+//         return
+//     }
+//     http.get(urls[turn],function(resp){
+//         resp.pipe(bl(function(err,data){
+//             if(err){
+//                 console.error(`error:${err.message}`)
+//             }
+//             total.push( data+'' )
+//             httpGet(--turn)
+//         }))
+//     })
+// }
+// httpGet(2)
