964ee1b7c04abacbbcde07f45c7bbcca6655e575
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-06 15:11:21 +0100

    up

--- staff/adrian-zhu/learnyounode/08- HTTP Collect/httpCollect.js
+++ staff/adrian-zhu/learnyounode/08- HTTP Collect/httpCollect.js
@@ -10,15 +10,3 @@ http.get(process.argv[2], (response) => {
         console.log(result);
     });
 });
-
-// const http = require('http')
-// const bl = require('bl')
-
-// http.get(process.argv[2], function (response) {
-//     response.pipe(bl(function (err, data) {
-//         if (err) return console.error(err)
-//         data = data.toString()
-//         console.log(data.length)
-//         console.log(data)
-//     }))
-// })
