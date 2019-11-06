3de73f244695daf84418257ce87ff7840d8a3a3e
Javier Sevilla <xaviersevilla@gmail.com>
2019-11-06 15:04:12 +0100

    finish learnyounode

--- staff/javier-sevilla/learnyounode/servidor-tiempo/index.js
+++ staff/javier-sevilla/learnyounode/servidor-tiempo/index.js
@@ -1,14 +1,19 @@
-const net = require('net')
-console.log(process.argv)
-const port = process.argv[2] 
-const server = net.createServer((socket)=> {
-    sconst date = new Date,
-    year = date.getFullYear(),
-    month = date.getMonth(), // empieza en 0
-    day = date.getDate(), // devuelve día del mes, empieza en 1
-    hours = date.getHours(),
-    inutes = date.getMinutes()
-    socket.end()
+const net = require('net'),
+    { argv: [, , port = 8080] } = process
+    debugger
+const server = net.createServer(socket => {
+    const date = new Date,
+        year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate(),
+        hours = date.getHours(),
+        minutes = date.getMinutes()
 
+    socket.end(`${year}-${d2(month)}-${d2(day)} ${d2(hours)}:${d2(minutes)}\n`)
 })
+
+function d2(num) {
+    return num < 10 ? `0${num}` : num
+}
+
 server.listen(port)
