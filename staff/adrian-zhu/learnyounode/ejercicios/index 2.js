3f61079bf09c151e2fe1756cb71dd6183f389ea1
AdriaanZhu <qtzhuzhu@gmail.com>
2019-11-06 08:58:49 +0100

    up

--- staff/adrian-zhu/learnyounode/ejercicios/index 2.js
+++
@@ -1,8 +0,0 @@
-const deley = require('./delaly')
-const salute = require('./salute')
-
-const {argv: [,,mills, lang, name] } = process
-
-// node . 3000 en juanito
-
-delay(Number(mills), ()=> console.log(salute(lang, name)))
--- staff/adrian-zhu/learnyounode/ejercicios/salute.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(lang, name){
-    switch(lang){
-        case 'en':
-            return `Hello, ${name}`
-            
-        case 'es':
-            return `Hola, ${name}`
-    }
-}
