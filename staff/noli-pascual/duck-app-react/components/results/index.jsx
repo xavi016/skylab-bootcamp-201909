123c659da20ca007c9e97c4ee5cde35452aeb814
Manoli 2013 <websitesbypol@gmail.com>
2019-11-07 19:35:41 +0100

    delete

--- staff/noli-pascual/duck-app-react/components/results/index.jsx
+++
@@ -1,5 +0,0 @@
-function Results({ items, onItemRender }) {
-    return <ul className="results">
-        {items.map(item => onItemRender(item))}
-    </ul>
-}
--- staff/noli-pascual/duck-app-react/utils/errors.js
+++
@@ -1,9 +0,0 @@
-class ContentError extends Error {
-    constructor(message) {
-        super(message)
-
-        Error.captureStackTrace(this, ContentError)
-
-        this.name = ContentError.name
-    }
-}
