a741d0c950f2b49be9dfdc6519ec1c0d02e96bb2
Manuel Barzi <manuelbarzi@gmail.com>
2019-11-07 10:23:40 +0100

    boost duck-app-express

--- staff/manuel-barzi/duck-app-express/components/landing/index.js
+++ staff/manuel-barzi/duck-app-express/components/landing/index.js
@@ -1,6 +1,6 @@
-module.exports = function({ register }) {
+module.exports = function({ register, login }) {
     return `<section class="view landing">
     <h1 class="landing__title">Welcome to Duck App 🐥</h1>
-    <p class="landing__options">Please, proceed to <a href="${register}">Register</a> or <a href="#">Login</a>.</p>
+    <p class="landing__options">Please, proceed to <a href="${register}">Register</a> or <a href="${login}">Login</a>.</p>
 </section>`
 }
--- staff/manuel-barzi/duck-app-express/components/register/index.js
+++ staff/manuel-barzi/duck-app-express/components/register/index.js
@@ -1,8 +1,8 @@
 const Feedback = require('../feedback')
 
-module.exports = function () {
+module.exports = function ({ path }) {
     return `<section class="view register">
-    <form method="post" action="/register">
+    <form method="post" action="${path}">
         <h1 class="register__title">Register</h1>
         <input class="register__field" type="text" name="name" placeholder="name">
         <input class="register__field" type="text" name="surname" placeholder="surname">
